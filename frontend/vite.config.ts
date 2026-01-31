a3288247febd817cd4f8ef4ba237dea8a2962b89
zhipeng <rongzhipeng@outlook.com>
2026-01-31 14:18:40 +0800

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
@@ -16,15 +15,6 @@ export default defineConfig({
         ],
       },
     }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
-    }), 
     tsconfigPaths()
   ],
   server: {
