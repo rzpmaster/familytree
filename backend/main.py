8c12e5b1e362416801dc7fad0ca33439f3525db3
zhipeng <rongzhipeng@outlook.com>
2026-02-23 14:13:14 +0800

    fix bug

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,6 @@
-import uvicorn
-
-
 def main():
     # uv run uvicorn app.main:app --reload
     print("Hello from backend!")
-    uvicorn.run(
-        "app.main:app",
-        reload=True,
-        host="127.0.0.1",
-        port=8000,
-    )
 
 
 if __name__ == "__main__":
